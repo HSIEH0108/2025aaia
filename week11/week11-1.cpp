a402938b700a7c52c9473cd812eb02811ed33d5e
HSIEH0108 <ericzongen0108@gmail.com>
2025-05-02 11:52:07 +0800

    week11
    week11

---
+++ week11/week11-1.cpp
@@ -0,0 +1,20 @@
+///week11-1.cpp
+///ㄧΑ/ㄧ计 function
+#include <stdio.h>
+///玡﹚竡ㄧΑ
+///癳 int 俱计
+///癳秈俱计a,俱计b
+
+int addnum(int a,int b)
+{
+    printf("ㄧΑaddnum()柑眔把计a:%d\n",a,b);
+    int ans = a + b;
+    return ans;
+}
+int main()
+{ ///璶ㄧΑ祘Α
+    printf(" main() ㊣ㄏノ addnum() \n");
+    int ans=addnum(2,3);
+    printf("main()眔氮:%d\n",ans);
+
+}
